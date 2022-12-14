3ea342fa98d766599c9feb70a16dd885b0b8e9be
Yale Araujo <yale850@gmail.com>
2022-12-14 10:07:09 -0300

    feat: controller using get and post routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { HttpModule } from './http.module';
-import { MailService } from './mail/mail.service';
-import { PostmarkMailService } from './mail/postmark-mail.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [AppController],
-  providers: [
-    {
-      provide: MailService,
-      useClass: PostmarkMailService,
-    },
-  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
