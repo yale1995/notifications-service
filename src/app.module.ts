566a997f8cfb996b621393a3d7914839495d6685
Yale Araujo <yale850@gmail.com>
2022-12-13 22:46:45 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { HttpModule } from './http.module';
+import { MailService } from './mail/mail.service';
+import { PostmarkMailService } from './mail/postmark-mail.service';
+
+@Module({
+  imports: [HttpModule],
+  controllers: [AppController],
+  providers: [
+    {
+      provide: MailService,
+      useClass: PostmarkMailService,
+    },
+  ],
+})
+export class AppModule {}
