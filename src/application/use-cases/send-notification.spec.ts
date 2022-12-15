f3d8eb302af202a529a81d31d457367151aed61f
Yale Araujo <yale850@gmail.com>
2022-12-15 09:25:02 -0300

    feat: creating and testins a notification repo

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,14 +1,9 @@
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
 import { SendNotification } from './send-notification';
-import { Notification } from '../entities/notification';
-
-const notificationsRepository = {
-  async create(notification: Notification) {
-    console.log(notification);
-  },
-};
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
     const sendNotification = new SendNotification(notificationsRepository);
 
     const { notification } = await sendNotification.execute({
@@ -17,6 +12,7 @@ describe('Send notification', () => {
       recipientId: 'example-recipient-id',
     });
 
-    expect(notification).toBeTruthy;
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 });
